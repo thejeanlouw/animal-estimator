15b962e89a858449d484ca81aaadab4a0b7c5888
Jean Louw <jlouw@iqbusiness.net>
2020-09-30 15:07:35 +0200

    Updates

---
+++ precache-manifest.55abfcec54259e54cca60fb7d1a43949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9ceb250e7adb4d8a7998f379afb153",
+    "url": "/annml-farm/index.html"
+  },
+  {
+    "revision": "d876f843ceb86cd44a7e",
+    "url": "/annml-farm/static/css/main.520ef911.chunk.css"
+  },
+  {
+    "revision": "a7791f5f596e8c1fbd08",
+    "url": "/annml-farm/static/js/2.2f162f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/annml-farm/static/js/2.2f162f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d876f843ceb86cd44a7e",
+    "url": "/annml-farm/static/js/main.593fa396.chunk.js"
+  },
+  {
+    "revision": "5acb7531264a00f50cbd",
+    "url": "/annml-farm/static/js/runtime-main.22fac724.js"
+  }
+]);
